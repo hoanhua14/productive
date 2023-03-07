1cb286e9eae61f7ecfe2eea0b626e3256fffc3f6
So Hoan Hua <huasohoan@gmail.com>
2023-03-06 19:26:42 -0800

    Feature 8 complete

--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from projects.models import Project
 
 
+@login_required
 def project_list(request):
-    projects = Project.objects.all()
+    projects = Project.objects.filter(owner=request.user)
     context = {
         "projects": projects,
     }
